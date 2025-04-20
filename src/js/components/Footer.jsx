9150c5be2de29fe308f31763ca713507ac758089
Madeleyner Fernandez <129197262+Madeleyner@users.noreply.github.com>
2025-04-20 02:21:05 +0000

    PageConReact - MF

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center py-3 nt-5">
+      <p className="mb-00">Copyright © Your Website 2018</p>
+    </footer>
+  );
+}
+
+export default Footer;
